513e3371ca605ab441cf64f3476102c2d0706ad1
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-07-06 18:09:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,87 +1,37 @@
 #!/bin/sh
-# Exit codes
-#	1	general error
-#	2	unsupported OS
-#	3	unsupported CPU/OS bits
 
-verbose () {
-	echo "${green}$1${normal}"
-}
-error	() {
-	echo "${red}$1${normal}"
-}
+#--------------------------------------------------
+# Upgrade the packages
+#--------------------------------------------------
+echo -e "\n============== Update Server ======================="
+sudo apt update && sudo apt upgrade -y
 
-# check for color support
-if test -t 1; then
+#----------------------------------------------------
+# Set hostname
+#----------------------------------------------------
+sudo hostnamectl set-hostname 
 
-    # see if it supports colors...
-    ncolors=$(tput colors)
+#--------------------------------------------------
+# Set up the timezones
+#--------------------------------------------------
+# set the correct timezone on ubuntu
+timedatectl set-timezone Africa/Kigali
+timedatectl
 
-    if test -n "$ncolors" && test $ncolors -ge 8; then
-        bold="$(tput bold)"
-        underline="$(tput smul)"
-        standout="$(tput smso)"
-        normal="$(tput sgr0)"
-        black="$(tput setaf 0)"
-        red="$(tput setaf 1)"
-        green="$(tput setaf 2)"
-        yellow="$(tput setaf 3)"
-        blue="$(tput setaf 4)"
-        magenta="$(tput setaf 5)"
-        cyan="$(tput setaf 6)"
-        white="$(tput setaf 7)"
-    fi
-	verbose "Enabled color support"
-fi
+#----------------------------------------------------
+# Disable password authentication
+#----------------------------------------------------
+sudo sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/UsePAM yes/UsePAM no/' /etc/ssh/sshd_config 
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo service sshd restart
 
-# check to confirm running as root.
-if [ "$(id -u)" -ne "0" ]; then
-	error "$(basename "$0") must be run as root";
-	exit 1
-fi
+#----------------------------------------------------
+# Install Fusionpbx 
+#----------------------------------------------------
+sudo apt install -y git lsb-release sngrep
+sudo apt autoremove -y
 
-#Make ourselves executable next time we are run
-chmod +x $0
-
-#Os/Distro Check
-os_check=$(lsb_release -is)
-real_os=$os_check
-check_major_release=$(lsb_release -rs | cut -d. -f1)
-
-os_unsupported () {
-	echo " Your Operating System appears to be: "
-	lsb_release -a
-	error "Your Operating System is not currently supported... Exiting the install."
-	exit 2;
-}
-
-if [ $os_check = 'Raspbian' ]; then
-	echo "${yellow}Detected Raspbian, using Debian for compatibility${normal}"
-	os_check="Debian"
-fi
-if [ $os_check = 'Debian' ]; then
-	if [ $check_major_release -ge 8 ]; then
-		verbose "Removing the CD image from /etc/apt/sources.list"
-		sed -i '/cdrom:/d' /etc/apt/sources.list
-		verbose "Updating system before starting."
-		apt-get update && apt-get -y upgrade
-		verbose "Installing Git"
-		apt-get install -y git
-		cd /usr/src
-		verbose "Fetching Installer"
-		if [ -d /usr/src/fusionpbx-install.sh ]; then
-			cd /usr/src/fusionpbx-install.sh
-			git pull
-		else
-			git clone https://github.com/fusionpbx/fusionpbx-install.sh			
-		fi
-		cd /usr/src/fusionpbx-install.sh/debian
-		./install.sh $@
-	else
-		error "Although you are running $real_os we require version >= 8"
-		os_unsupported
-	fi
-else
-	os_unsupported
-fi
+cd /usr/src && git clone https://github.com/fusionpbx/fusionpbx-install.sh.git
+cd /usr/src/fusionpbx-install.sh/debian && ./install.sh
 
