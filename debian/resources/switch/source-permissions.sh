bec923af50bd2659ba60764e4120a987a3dbe5ff
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-07-06 18:10:25 +0200

    Delete debian directory

--- debian/resources/switch/source-permissions.sh
+++
@@ -1,4 +0,0 @@
-#setup owner and group, permissions and sticky
-chown -R www-data:www-data /usr/local/freeswitch
-chmod -R ug+rw /usr/local/freeswitch
-find /usr/local/freeswitch -type d -exec chmod 2770 {} \;
